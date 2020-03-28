448b5396920db6fe27bd87896d8d63403bb3c2fe
Reece Como <reece.como@healthengine.com.au>
2020-03-28 15:02:02 +0800

    fix lint

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 let package = Package(
     name: "XRouter",
     products: [
-        .library(name: "XRouter", targets: ["XRouter"]),
+        .library(name: "XRouter", targets: ["XRouter"])
     ],
     dependencies: [],
     targets: [
-        .target(name: "XRouter", dependencies: []),
+        .target(name: "XRouter", dependencies: [])
     ]
 )
